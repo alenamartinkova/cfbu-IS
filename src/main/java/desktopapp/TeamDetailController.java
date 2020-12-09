d1798be5787159696fc71b5a6f485e1da747e651
a.mar <martinkova.a@gmail.com>
2020-12-09 18:04:51 +0100

    use case 2 alternative

--- src/main/java/desktopapp/TeamDetailController.java
+++ src/main/java/desktopapp/TeamDetailController.java
@@ -46,22 +46,6 @@ public class TeamDetailController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) { }
 
-    public void openAllTeams(ActionEvent event) {
-        Parent root = null;
-        Stage stage = new Stage();
-        try {
-            URL url = new File("src/main/java/desktopapp/teams_all.fxml").toURI().toURL();
-            root = FXMLLoader.load(url);
-            stage.setScene(new Scene(root, 645, 501));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Stage oldWindow = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        oldWindow.close();
-        stage.setTitle("VIS TEAMS");
-        stage.show();
-    }
-
     public void openAllMatches(ActionEvent event) {
         Parent root = null;
         Stage stage = new Stage();
@@ -78,19 +62,26 @@ public class TeamDetailController implements Initializable {
         stage.show();
     }
 
-    public void onTeamSubmit(ActionEvent event) throws SQLException {
+    public void onTeamSubmit(ActionEvent event) throws SQLException, IOException {
         Integer doUpdate = Team.proceedUpdate(this.selectedTeam, this.quarantined_from.getText(), this.covid.getText());
 
+        Parent root = null;
+        URL url = null;
         if(doUpdate == 0) {
             openAllMatches(event);
         } else if (doUpdate == -1) {
-            
-        } else if (doUpdate == -2) {
 
+        } else if (doUpdate == -2) {
+            url = new File("src/main/java/desktopapp/inform_players.fxml").toURI().toURL();
         }
+
+        root = FXMLLoader.load(url);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 400, 150));
+        stage.show();
     }
 
-    public void handleButtons(ActionEvent event) {
+    public void handleButtons(ActionEvent event) throws IOException {
         String buttonID = ((Button)event.getSource()).getId();
         Parent root = null;
         Stage stage = new Stage();
@@ -121,6 +112,36 @@ public class TeamDetailController implements Initializable {
                 oldWindow.close();
                 stage.setTitle("VIS TEAMS");
                 stage.show();
+                break;
+            case "no":
+                try {
+                    URL url = new File("src/main/java/desktopapp/stop_matches.fxml").toURI().toURL();
+                    root = FXMLLoader.load(url);
+                    stage.setScene(new Scene(root, 645, 501));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                stage.setScene(new Scene(root, 400, 150));
+                stage.show();
+                oldWindow = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                oldWindow.close();
+                break;
+            case "yes":
+                Team.storeInfo();
+                try {
+                    URL url = new File("src/main/java/desktopapp/stop_matches.fxml").toURI().toURL();
+                    root = FXMLLoader.load(url);
+                    stage.setScene(new Scene(root, 645, 501));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                stage.setScene(new Scene(root, 400, 150));
+                stage.show();
+
+                oldWindow = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                oldWindow.close();
                 break;
         }
     }
